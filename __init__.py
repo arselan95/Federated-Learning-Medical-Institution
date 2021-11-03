8e09a1d3a82a018acc30d8f011c9f7b939ff7398
Arslan <arselan95@gmail.com>
2021-11-03 15:47:15 -0700

    update login function

--- __init__.py
+++ __init__.py
@@ -10,7 +10,24 @@ conn=None
 @app.route("/login", methods = ['POST', 'GET'])
 def login():
 	if request.method=='GET':
-		return render_template('loginpage.html')
+		return render_template('loginpage.html', data="")
+	if request.method=="POST":
+		user=request.form['username']
+		password=request.form['password']
+		user=user.replace(" ","")
+		password=password.replace(" ","")
+		user=str(user)
+		password=str(password)
 
+		if(user=="node2institution" and password=="node2123"):
+			print("here")
+			return "correct credentials - login node 2"
+
+		if(user=="node3institution" and password=="node3123"):
+			return "correct credentials - login node 3"
+
+
+		return render_template('loginpage.html', data="wrong credentials")
+		
 if __name__ == "__main__":
     app.run()
