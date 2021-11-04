ef45c72a09659381b5c08f91c5d1e8a5d95db328
Arslan <arselan95@gmail.com>
2021-11-03 20:09:47 -0700

    sessions added

--- __init__.py
+++ __init__.py
@@ -1,15 +1,17 @@
-from flask import Flask,request,redirect,url_for,render_template
+from flask import Flask,request,redirect,url_for,render_template,session
 import subprocess
 import pymysql
 import requests
 
 app = Flask(__name__)
 conn=None
+app.secret_key = 'super secret key'
 #conn==pymysql.connect()
 
 @app.route("/login", methods = ['POST', 'GET'])
 def login():
 	if request.method=='GET':
+		session.clear()
 		return render_template('loginpage.html', data="")
 	if request.method=="POST":
 		user=request.form['username']
@@ -21,13 +23,40 @@ def login():
 
 		if(user=="node2institution" and password=="node2123"):
 			print("here")
-			return "correct credentials - login node 2"
+			session['node2session']="1"
+			if 'node2session' in session:
+				print(session['node2session'])
+
+			return render_template('sessiontest.html')
 
 		if(user=="node3institution" and password=="node3123"):
-			return "correct credentials - login node 3"
+			session['node3session']="2"
+			if 'node3session' in session:
+				print(session['node3session'])
+			return render_template('sessiontest.html')
 
 
-		return render_template('loginpage.html', data="wrong credentials")
+		else:
+			return render_template('loginpage.html', data="wrong credentials")
+
+'''
+@app.route("/testsession", methods = ['POST', 'GET'])
+def testsession():
+	if request.method=="GET":
+		print(session)
+		return "session"
+	if request.method=="POST":
+
+		print(session)
+
+		if 'node3session' in session:
+			print("session3")
+			return "node3"
 		
+		if 'node2session' in session:
+			print("session2")
+			return "node2"
+'''
+			
 if __name__ == "__main__":
     app.run()
